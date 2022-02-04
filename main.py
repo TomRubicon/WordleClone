bdf939fa71a8057282f7b3eabeb0e90630cc2f4a
John <johnmilanowski@gmail.com>
2022-02-04 17:08:58 -0500

    Add a basic way to catch repeat letters

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 from rich import print
+from collections import defaultdict
 import random
 
 def check_word(a, b):
     if len(a) != len(b):
         return 'Words are not the same length!'
-
+    a = a.lower()
     output = ''
     winCount = 0
     win = False
 
+    letters = defaultdict(int)
+    letters_used = defaultdict(int)
+
+    for i in b:
+        letters[i] += 1
+
+    print(letters)
+
     for i in range(len(b)):
-        if a[i] == b[i]: 
+        if a[i] == b[i]:
             output += f'[bold green]|{a[i].upper()}|[/bold green] '
             winCount += 1
-        elif a[i] in b:
+            letters_used[a[i]] += 1
+        elif a[i] in b and letters_used[a[i]] < letters[a[i]]:
             output += f'[bold yellow]|{a[i].upper()}|[/bold yellow] '
+            letters_used[a[i]] += 1
         else: 
             output += f'[bold gray]|{a[i].upper()}|[/bold gray] '
     
@@ -27,6 +38,7 @@ def print_board(gb):
     print('\n[underline bold]      WORDLE[/underline bold]')
     print(f'    Guess: {turn}/6\n')
 
+    print(' 1   2   3   4   5')
     for i in gb:
         print(''.join(i))
 
@@ -39,12 +51,15 @@ game_board = [[ '| | ', '| | ', '| | ', '| | ', '| | '],
 
 running = True
 turn = 1
-solution = random.choice(open('words.txt').readlines())[:-1]
+# solution = random.choice(open('words.txt').readlines())[:-1]
+solution = 'aaabb'
+print(f'\n Solution: {solution}')
 
 while running:
     print_board(game_board)
 
-    print('\nEnter your guess (q! to exit): ')
+    print('\nEnter your guess (q! to exit):')
+    print('12345')
     guess = input()
     print('\n')
 
@@ -55,10 +70,10 @@ while running:
         print('Guess must be 5 letters')
         continue
 
-    game_board[turn-1] = check_word(guess.lower(), solution)[0]
+    game_board[turn-1] = check_word(guess, solution)[0]
     turn += 1
 
-    if(check_word(guess.lower(), solution)[1]):
+    if(check_word(guess, solution)[1]):
         print_board(game_board)
         print(f'\nCongratulations! [bold green]{solution.upper()}[/bold green] was the word!')
         break
