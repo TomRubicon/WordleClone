eadbfd074d12cc86c4b0401c227ef58465d66e69
John <johnmilanowski@gmail.com>
2022-02-04 11:56:09 -0500

    Add win and lose states

--- main.py
+++ main.py
@@ -6,16 +6,29 @@ def check_word(a, b):
         return 'Words are not the same length!'
 
     output = ''
+    winCount = 0
+    win = False
 
     for i in range(len(b)):
         if a[i] == b[i]: 
             output += f'[bold green]|{a[i]}|[/bold green] '
+            winCount += 1
         elif a[i] in b:
             output += f'[bold yellow]|{a[i]}|[/bold yellow] '
         else: 
             output += f'[bold gray]|{a[i]}|[/bold gray] '
+    
+    if winCount >= 5:
+         win = True
 
-    return output
+    return output, win
+
+def print_board(gb):
+    print('\n[underline bold]      WORDLE[/underline bold]')
+    print(f'    Guess: {turn}/6\n')
+
+    for i in gb:
+        print(''.join(i))
 
 game_board = [[ '| | ', '| | ', '| | ', '| | ', '| | '],
               [ '| | ', '| | ', '| | ', '| | ', '| | '],
@@ -28,22 +41,30 @@ running = True
 turn = 1
 solution = 'raged'
 
-print(check_word('farty', 'faryi'))
-
-
 while running:
-    print('\n[underline bold]      WORDLE[/underline bold]')
-    print(f'    Guess: {turn}/6\n')
+    print_board(game_board)
 
-    for i in game_board:
-        print(''.join(i))
-
-    print('\nEnter your guess: ')
+    print('\nEnter your guess (q! to exit): ')
     guess = input()
+    print('\n')
+
+    if(guess == "q!"):
+        break
 
     if len(guess) > 5 or len(guess) < 5 or guess.isalpha() != True:
         print('Guess must be 5 letters')
         continue
 
-    game_board[turn-1] = check_word(guess.lower(), solution)
+    game_board[turn-1] = check_word(guess.lower(), solution)[0]
     turn += 1
+
+    if(check_word(guess.lower(), solution)[1]):
+        print_board(game_board)
+        print(f'\nCongratulations! [bold green]{solution}[/bold green] was the word!')
+        break
+
+    if turn > 6:
+        turns = 6
+        print_board(game_board)
+        print(f'\nThe game is over. The word was: [bold red]{solution}[/bold red]!')
+        break
